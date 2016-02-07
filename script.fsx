250bcc7c4301e1224cda2856b61302b98a459f48
Tomas Petricek <tomas@tomasp.net>
2016-02-07 00:58:00 +0000

    Reading Azure storage data

---
+++ script.fsx
@@ -0,0 +1,26 @@
+﻿#load "config.fsx"
+#load "packages/FsLab/FsLab.fsx"
+#load "packages/FSharp.Azure.StorageTypeProvider/StorageTypeProvider.fsx"
+open System
+open FSharp.Data
+open Microsoft.WindowsAzure.Storage
+
+// Sending sample request to the logging service
+Http.RequestString
+  ( "http://coeffectlogs.azurewebsites.net/log", httpMethod="POST", 
+    body=HttpRequestBody.TextRequest "testing..." )
+
+// Read the logs from Azure storage
+let account = CloudStorageAccount.Parse(Connection.CoeffectLogStorage)
+let client = account.CreateCloudBlobClient()
+let logs = client.GetContainerReference("logs")
+
+// List all log files
+for log in logs.GetDirectoryReference("").ListBlobs() do
+  printfn " - %s" (Seq.last log.Uri.Segments) 
+
+// List all log files & read their contents
+for log in logs.GetDirectoryReference("").ListBlobs() do
+  printfn "\n\n%s\n%s" (Seq.last log.Uri.Segments) (Seq.last log.Uri.Segments |> String.map (fun _ -> '-'))
+  logs.GetAppendBlobReference(log.Uri.Segments |> Seq.last).DownloadText() |> printfn "%s"
+
